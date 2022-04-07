7a01fb0db83f73bbd6c68eb510b54e01bd126ff1
vladimir <grun_gespenst@tut.by>
2022-04-07 19:43:17 +0300

    idea

--- myalbum/app/views.py
+++ myalbum/app/views.py
@@ -7,13 +7,21 @@ from .forms import AddForm
 def resize(request):
     if request.method == 'POST':
         form = AddForm(request.POST, request.FILES)
+        print(request.POST['width'])
+        # print(request.FILES)
         if form.is_valid():
+
+            if request.POST['width'] and request.POST['height']:
+                width, height = request.POST['width'], request.POST['height']
+            else:
+                pass
 
 
             form.save()
 
             img_obj = form.instance
-            return render(request, 'app/resize.html', {'form': form, 'img_obj': img_obj})
+            return render(request, 'app/resize.html', {'form': form, 'img_obj': img_obj,
+                                                       'width':width,'height':height })
     else:
         form = AddForm()
     return render(request, 'app/resize.html', {'form': form})
