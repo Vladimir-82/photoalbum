ff297da97082749e6999d1449463bccca41abb47
vladimir <grun_gespenst@tut.by>
2022-04-17 21:16:52 +0300

    all works

--- myalbum/app/urls.py
+++ myalbum/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import resize
+from .views import resize, ApiResize
 
 urlpatterns = [
     path('resize', resize, name='resize'),
+    path('api/resize', ApiResize.as_view()),
 ]
--- myalbum/app/views.py
+++ myalbum/app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.core.files.base import ContentFile
 from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.response import Response
 
 from .forms import AddForm
 from .models import App
+from .serializers import AppSerializer
 
 from PIL import Image
 from io import BytesIO
@@ -16,7 +19,6 @@ def resize(request):
     if request.method == 'POST':
         form = AddForm(request.POST, request.FILES)
         photo = request.FILES['photo']
-
 
         if form.is_valid():
             sizes = []
@@ -34,19 +36,14 @@ def resize(request):
                 width_new = request.POST['width']
                 height_new = (int(width) / int(request.POST['width'])) * int(height)
 
-
             form.save()
             photo_obj = App.objects.latest('created_at')
-
-
 
             im = image.resize((int(width_new), int(height_new)), Image.ANTIALIAS)
             buffer = BytesIO()
             im.save(fp=buffer, format='webp')
             hash = uuid.uuid3(uuid.NAMESPACE_DNS, photo_obj.title)
-            print(hash)
             name = f'{hash}_{width_new}x{height_new}.webp'
-            print(name)
             photo_obj.photo_mod.save(name=name, content=ContentFile(buffer.getvalue()), save=False)
             photo_obj.save()
             if photo_obj.photo_mod.size in sizes:
@@ -56,3 +53,50 @@ def resize(request):
     else:
         form = AddForm()
     return render(request, 'app/resize.html', {'form': form})
+
+
+
+
+class ApiResize(generics.ListCreateAPIView):
+    queryset = App.objects.all()
+    serializer_class = AppSerializer
+
+    def post(self, request, *args, **kwargs):
+        photo = request.data['photo']
+        print(photo, 'photo')
+
+
+        sizes = []
+        photos = App.objects.all()
+        for ph in photos:
+            sizes.append(ph.photo_mod.size)
+        print(photos, 'photos')
+        serializer = AppSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+        image = Image.open(photo)
+        width, height = image.size
+        if request.POST['width'] and request.POST['height']:
+            width_new, height_new = request.POST['width'], request.POST['height']
+
+        else:
+            width_new = request.POST['width']
+            height_new = (int(width) / int(request.POST['width'])) * int(height)
+
+
+        photo_obj = App.objects.latest('created_at')
+        print(photo_obj, 'photo_obj')
+
+        im = image.resize((int(width_new), int(height_new)), Image.ANTIALIAS)
+        buffer = BytesIO()
+        im.save(fp=buffer, format='webp')
+        hash = uuid.uuid3(uuid.NAMESPACE_DNS, photo_obj.title)
+        name = f'{hash}_{width_new}x{height_new}.webp'
+        photo_obj.photo_mod.save(name=name, content=ContentFile(buffer.getvalue()), save=False)
+        photo_obj.save()
+
+        if photo_obj.photo_mod.size in sizes:
+            return HttpResponse('You have already converted this file!!!')
+
+        return Response(photo_obj.photo_mod.url)
